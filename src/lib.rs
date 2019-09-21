33063148bcf2825011aad6004232a5d622651d46
Frank Denis <github@pureftpd.org>
2019-09-21 15:56:14 +0200

    Initial import

---
+++ src/lib.rs
@@ -0,0 +1,82 @@
+use std::env::set_current_dir;
+use std::fs::File;
+use std::fs::OpenOptions;
+use std::io::prelude::*;
+use std::os::unix::io::AsRawFd;
+use std::path::PathBuf;
+
+#[derive(Default, Debug, Clone)]
+pub struct Daemonize {
+    pub chdir: Option<PathBuf>,
+    pub pid_file: Option<PathBuf>,
+    pub stdin_file: Option<PathBuf>,
+    pub stdout_file: Option<PathBuf>,
+    pub stderr_file: Option<PathBuf>,
+    pub umask: Option<libc::mode_t>,
+}
+
+impl Daemonize {
+    unsafe fn _doit(self) -> Result<(), &'static str> {
+        match libc::fork() {
+            -1 => return Err("fork() failed"),
+            0 => {}
+            _ => {
+                libc::_exit(0);
+            }
+        }
+        libc::setsid();
+        match libc::fork() {
+            -1 => return Err("fork() failed"),
+            0 => {}
+            _ => {
+                libc::_exit(0);
+            }
+        };
+        if let Some(umask) = self.umask {
+            libc::umask(umask);
+        }
+        if let Some(chdir) = self.chdir {
+            set_current_dir(chdir).map_err(|_| "chdir() failed")?;
+        }
+        let stdin_file = self.stdin_file.unwrap_or_else(|| "/dev/null".into());
+        let fd = OpenOptions::new()
+            .read(true)
+            .open(stdin_file)
+            .map_err(|_| "Unable to open the stdin file")?;
+        if libc::dup2(fd.as_raw_fd(), 0) == -1 {
+            return Err("dup2(stdin) failed");
+        }
+        let stdout_file = self.stdout_file.unwrap_or_else(|| "/dev/null".into());
+        let fd = OpenOptions::new()
+            .write(true)
+            .open(stdout_file)
+            .map_err(|_| "Unable to open the stdout file")?;
+        if libc::dup2(fd.as_raw_fd(), 1) == -1 {
+            return Err("dup2(stdout) failed");
+        }
+        let stderr_file = self.stderr_file.unwrap_or_else(|| "/dev/null".into());
+        let fd = OpenOptions::new()
+            .write(true)
+            .open(stderr_file)
+            .map_err(|_| "Unable to open the stderr file")?;
+        if libc::dup2(fd.as_raw_fd(), 2) == -1 {
+            return Err("dup2(stderr) failed");
+        }
+        if let Some(pid_file) = self.pid_file {
+            let pid = match libc::getpid() {
+                -1 => return Err("getpid() failed"),
+                pid => pid,
+            };
+            let pid_str = format!("{}", pid);
+            File::create(pid_file)
+                .map_err(|_| "Creating the PID file failed")?
+                .write_all(pid_str.as_bytes())
+                .map_err(|_| "Writing to the PID file failed")?;
+        }
+        Ok(())
+    }
+
+    pub fn doit(self) -> Result<(), &'static str> {
+        unsafe { self._doit() }
+    }
+}
